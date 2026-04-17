8cf62b674973e9aa1a3cd4b5cf3dd4e8df0badf5
Shailen Sobhee <shailen.sobhee@amd.com>
2026-04-18 01:55:48 +0200

    feat: add EVT_DISTANCE event constant for HC-SR04 sensor

--- custom_components/espsomfy_rts/const.py
+++ custom_components/espsomfy_rts/const.py
@@ -30,6 +30,7 @@ EVT_UPDPROGRESS = "updateProgress"
 EVT_WIFISTRENGTH = "wifiStrength"
 EVT_ETHERNET = "ethernet"
 EVT_MEMSTATUS = "memStatus"
+EVT_DISTANCE = "distance"
 
 ATTR_RESTOREFILE = "Restore File"
 ATTR_AVAILABLE_MODES = "???"
